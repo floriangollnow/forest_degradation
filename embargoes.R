956a4561bf01979e1feb3974c830073143ed7e88
florian gollnow <floriangollnow@gess-vpn-1-a-139.intern.ethz.ch>
2022-01-27 16:43:43 -0500

    cleaned

--- embargoes.R
+++ embargoes.R
@@ -1,11 +1,10 @@
 # read embargo area
 # extract polygons
-# if no poligon is provided or data is corrupted see if it contains a coordinate
-# if coordinats are provided, intersect with properties of car
+# if no polygon is provided or data is corrupted see if it contains a coordinate
+# if coordinates are provided, intersect with properties of car
 # add property polygon to embargoed area
 # 
-# two columnes: embargo start year, embargo end year
-
+# two columns: embargo start year, embargo end year
 
 #car data for MT, PA, RO. only use properties filter (tipo_imove=="IRU") excluding settlements
 #
@@ -23,7 +22,7 @@ emb_row <- emb %>% mutate (row=1:nrow(emb))
 
 emb_poly <- emb_row %>% mutate (Polygon=!is.na (GEOM_AREA_EMBARGADA))
 emb_poly <- emb_poly %>% filter (str_ends(GEOM_AREA_EMBARGADA, pattern = "[/)]"))# exclude fully corrupted
-# repair whats possible by adding 
+# repair whats possible by adding )
 emb_poly <- emb_poly %>% mutate (geometry = case_when (str_detect(GEOM_AREA_EMBARGADA, pattern = "[/(]{2}")&
                                                          str_ends(GEOM_AREA_EMBARGADA, pattern = "[/)]{2}", negate=T) ~ paste0(GEOM_AREA_EMBARGADA, ")"),
                                                        str_detect(GEOM_AREA_EMBARGADA, pattern = "[/(]{3}")&
@@ -41,11 +40,11 @@ emb_poly2 <- emb_poly2 %>% st_buffer(0)
 emb_poly2 <- st_make_valid(emb_poly2)
 emb_poly3 <- emb_poly2 %>% filter(st_is_valid(emb_poly2)) # only keep valid polygons
 
-## save dataset
+## save polygon data set from IBAMA
 write_rds (emb_poly3 , file.path(dir_data, "processing/emb_poly3.rds"))
 
 
-########
+######## which locations can be recuperated using CAR
 # which rows were excluded based on missing or corrupted polygons and do they have coordinates?
 ind_poly <- emb_poly3 %>% as_tibble () %>% select (-c(GEOM_AREA_EMBARGADA, WKS ))
 ind_poly <- ind_poly %>% mutate(poly=TRUE)
@@ -56,15 +55,14 @@ emb_2intersect <- emb_row_check %>%  filter(is.na (poly),!is.na(NUM_LATITUDE_TAD
 unique(emb_2intersect$poly)
 
 
-
 #####
-# convert coordinates to sf
+# convert IBAMA coordinates to sf
 # correct LAT/LONG confusion if there (if coordinates do not fit into Brazil, test if they would with turned LAT/LONG)
 
+# use brazil as bounding box 
 braz <- ne_countries(country="Brazil", scale = "medium", returnclass = "sf")
 st_crs(braz)
 braz_bb <- st_bbox(braz)
-
 
 ## check for confusion between long and lat
 # check if lon, lat fit into bounding box (Brazil), if yes, leave as is, if not check if they would fit if lon=lat and lat=lon, if yes, change, otherwise NA
@@ -84,7 +82,9 @@ emb_2intersect_latlong_sf <- emb_2intersect_latlong %>%
 ggplot(emb_2intersect_latlong_sf )+geom_sf(aes())
 
 
-### find intersecting properties for MT, PA, RO
+
+### 
+### intyersect points from IBAMA with CAR properties for MT, PA, RO
 
 # MT 
 MT_car <- read_sf (file.path(car_data, "uf_MT/uf_MT.shp"))
@@ -94,8 +94,8 @@ MT_box <- MT_car %>% st_bbox()
 MT_car_p <- MT_car %>% filter (tipo_imove=="IRU")
 MT_emb_2intersect_latlong_sf <- emb_2intersect_latlong_sf %>%  st_crop (MT_box)
 # intersect
-MT_car_embargoed <- sf::st_intersection ( MT_car %>% select(id),  MT_emb_2intersect_latlong_sf) 
-MT_car_embargoed_poly <- MT_car %>% select(id) %>% inner_join(as_tibble (MT_car_embargoed )%>% select(-geometry), by="id")# keep polygons, not points!
+MT_car_embargoed <- sf::st_intersection ( MT_car_p %>% select(id),  MT_emb_2intersect_latlong_sf) 
+MT_car_embargoed_poly <- MT_car_p %>% select(id) %>% inner_join(as_tibble (MT_car_embargoed )%>% select(-geometry), by="id")# keep polygons, not points!
 write_rds (MT_car_embargoed_poly, file.path(dir_data, "processing/MT_car_embargoed.rds"))
 
 # PA
@@ -106,8 +106,8 @@ PA_box <- PA_car %>% st_bbox()
 PA_car_p <- PA_car %>% filter (tipo_imove=="IRU")
 PA_emb_2intersect_latlong_sf <- emb_2intersect_latlong_sf %>%  st_crop (PA_box)
 # intersect
-PA_car_embargoed <- st_intersection (PA_car, PA_emb_2intersect_latlong_sf) 
-PA_car_embargoed_poly <- PA_car %>% select(id) %>% inner_join(as_tibble (PA_car_embargoed )%>% select(-geometry), by="id")
+PA_car_embargoed <- st_intersection (PA_car_p, PA_emb_2intersect_latlong_sf) 
+PA_car_embargoed_poly <- PA_car_p %>% select(id) %>% inner_join(as_tibble (PA_car_embargoed )%>% select(-geometry), by="id")
 write_rds (PA_car_embargoed_poly, file.path(dir_data, "processing/PA_car_embargoed.rds"))
 
 # RO 
@@ -118,8 +118,8 @@ RO_box <- RO_car %>% st_bbox()
 RO_car_p <- RO_car %>% filter (tipo_imove=="IRU")
 RO_emb_2intersect_latlong_sf <- emb_2intersect_latlong_sf %>%  st_crop (RO_box)
 # intersect
-RO_car_embargoed <- st_intersection (RO_car, RO_emb_2intersect_latlong_sf) 
-RO_car_embargoed_poly <- RO_car %>% select(id) %>% inner_join(as_tibble (RO_car_embargoed )%>% select(-geometry), by="id")
+RO_car_embargoed <- st_intersection (RO_car_p, RO_emb_2intersect_latlong_sf) 
+RO_car_embargoed_poly <- RO_car_p %>% select(id) %>% inner_join(as_tibble (RO_car_embargoed )%>% select(-geometry), by="id")
 write_rds (RO_car_embargoed_poly, file.path(dir_data, "processing/RO_car_embargoed.rds"))
 
 ### combine polygon data keeping only the the id and row
@@ -127,13 +127,15 @@ CAR_embargoe <-  rbind(MT_car_embargoed_poly %>% select (id, row), PA_car_embarg
 CAR_embargoe_row <- CAR_embargoe %>% select (row)# sometimes multiple polygons per row id -> keep the larger
 CAR_embargoe
 
+
+#############################
 #add polysi from embargo data
 CAR_embargoe_all <- CAR_embargoe %>% select(row) %>% rbind(emb_poly3 %>% select(row)) %>% arrange (row)
 write_rds (CAR_embargoe_all, file.path(dir_data, "processing/Car_embargoed_all.rds"))
 
 ########################
-# add embargo area
-CAR_embargoe_all_data <- CAR_embargoe_all %>% left_join (emb_row, by="row") 
+# add embargod polygons - data includes embargoed polygons from Ibama and if ibama provided coordinates, but no polygon, the intersecting CAR polygon
+CAR_embargoe_all_data <- CAR_embargoe_all %>% mutate(source="CAR") %>% left_join (emb_row %>% mutate (source="ibama"), by="row") 
 write_rds (CAR_embargoe_all_data, file.path(dir_data, "result/Car_embargoed_all_data.rds"))
 write_sf (CAR_embargoe_all_data, file.path(dir_data, "result/Car_embargoed_all_data.geojson"))
 
