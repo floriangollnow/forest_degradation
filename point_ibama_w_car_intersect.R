fe27724c1705e96780c40d4f63d77164e6d95288
floriangollnow <fgollnow@bu.edu>
2022-01-27 22:59:21 +0000

    embargoes

--- point_ibama_w_car_intersect.R
+++ point_ibama_w_car_intersect.R
@@ -2,6 +2,7 @@
 
 library (tidyverse)
 library (sf)  
+sf::sf_use_s2(FALSE)# otherwise geometry issues - and assuming planar LAT/LONG seems fine to me
 library(lubridate)
 
 dir_data <- "~/Data/Points"
@@ -17,6 +18,10 @@ names(embargoes)
 embargoes_date <- embargoes %>% select(DAT_EMBARGO, DAT_DESEMBARGO) %>%  mutate(embargoStartY = year(dmy_hms(DAT_EMBARGO)) ,
          embargoEndY = year(dmy_hms(DAT_DESEMBARGO))) 
 
+
 point_emb <- point %>%  st_intersection(embargoes_date)
-write_rds(file.path(point_emb, "Point_car_embargoed_allY.rds"))
-write_sf(file.path(point_emb, "Point_car_embargoed_allY.geojson"))
+
+point_emb_all <- point %>% left_join(point_emb %>% as_tibble() %>% select(-geometry), by="row")
+
+write_rds(point_emb_all,file.path(dir_emb, "Point_car_embargoed_allY.rds"))
+write_sf(point_emb_all, file.path(dir_emb, "Point_car_embargoed_allY.geojson"))
