4bde88b6dbc4fe38ca87a77ad51843dde1de142a
florian gollnow <floriangollnow@florians-MacBook-Pro.local>
2022-01-28 10:56:19 -0500

    updated osm

--- osm_StreetsBrazil.R
+++ osm_StreetsBrazil.R
@@ -1,38 +1,52 @@
 #osm roads
 
-if(!require("osmdata")) install.packages("osmdata")
-if(!require("tidyverse")) install.packages("tidyverse")
+if(!require("osmdata")) install.packages("osmdata")if(!require("tidyverse")) install.packages("tidyverse")
 if(!require("sf")) install.packages("sf")
 if(!require("ggmap")) install.packages("ggmap")
+devtools::install_github("ropensci/rnaturalearthdata")
+devtools::install_github("ropensci/rnaturalearthhires")
 #load packages
 library(tidyverse)
 library(osmdata)
 library(sf)
-library(ggmap)
+#library(ggmap)
 
 library(rnaturalearth)
+library(rnaturalearthhires)
+
 dir_data <- "~/Data/roads"
+
 head(available_features())
 (available_tags("highway"))
 ?getbb
- <- getbb("Novo Progresso",featuretype = "settlement" )
-
+test <- getbb("Cuiaba",featuretype = "settlement" )
+str(test)
 state <- ne_states(country="Brazil", returnclass="sf") 
-state <- state %>% filter (code_hasc=="BR.PA")#| code_hasc=="BR.MT"| code_hasc=="BR.RO")
+state <- state %>% filter (code_hasc=="BR.PA"| code_hasc=="BR.MT"| code_hasc=="BR.RO")
 bb_box_state <- st_bbox(state) 
 bb_box_state.m <- matrix (bb_box_state, byrow = F, ncol = 2)
+rownames(bb_box_state.m)<- c("x","y")
+colnames(bb_box_state.m)<- c("min","max")
 
+str(bb_box_state.m)
 
-q <- bb_box_state.m %>%
+q <- bb_box_state.m%>%
   opq()%>%
   add_osm_features(
     features = c ("\"highway\"=\"motorway\"",
                   "\"highway\"=\"trunk\"",
-                  "\"highway\"=\"primary\""))
+                  "\"highway\"=\"primary\"",
+                  "\"highway\"=\"secondary\"",
+                  "\"highway\"=\"tertiary\"",
+                  "\"highway\"=\"unclassified\"",
+                  "\"highway\"=\"track\"",
+                  "\"highway\"=\"road\""))
 
-all_roads2 <- osmdata_sf(q)
+all_roads <- osmdata_sf(q)
 
-all_roads <- st_to_sf (all_roads)
+all_roads_lines <- (all_roads$osm_lines)
+
+
 write_rds (all_roads, file.path(dir_data, "OSM_roads.rds"))
 write_sf (all_roads, file.path(dir_data, "OSM_roads.geojson"))
 # 
@@ -40,8 +54,4 @@ write_sf (all_roads, file.path(dir_data, "OSM_roads.geojson"))
 
 
 # ,
-# "\"highway\"=\"secondary\"",
-# "\"highway\"=\"tertiary\"",
-# "\"highway\"=\"unclassified\"",
-# "\"highway\"=\"track\"",
-# "\"highway\"=\"road\"")
+
