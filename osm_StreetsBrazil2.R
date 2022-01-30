7d9c1182d5329c06deca2a2cac438ff3325a5131
florian gollnow <floriangollnow@gess-vpn-1-a-118.intern.ethz.ch>
2022-01-29 21:54:14 -0500

    osm roads done

--- osm_StreetsBrazil2.R
+++ osm_StreetsBrazil2.R
@@ -1,18 +1,16 @@
 # osm roads
-# date January 28 2022
+# date January 29 2022
 # roads for MT, PA, RO
 # author: florian gollnow
 
 #devtools::install_github("ropensci/rnaturalearthdata")
 #devtools::install_github("ropensci/rnaturalearthhires")
-library(httr)    
-set_config(use_proxy(url="10.3.100.207",port=8080))
+
 #load packages
 library(tidyverse)
 library(osmdata)
 library(sf)
-remotes::install_github("MatthewJWhittle/spatialutils")
-library(spatialutils)
+
 #library(ggmap)
 
 library(rnaturalearth)
@@ -31,9 +29,10 @@ state <- ne_states(country="Brazil", returnclass="sf")
 state <- state %>% filter (code_hasc=="BR.PA"| code_hasc=="BR.MT"| code_hasc=="BR.RO")
 ggplot()+geom_sf(data=state)
 bb_box_state <- st_bbox(state) 
-bb_box_state_tile <- split_bbox(bb_box_state,4,4)
+bb_box_state_tile <- split_bbox(bb_box_state,4,4)# see funtion in file x_split_box.R
 
-for (i in 1:length(bb_box_state_tile)){
+# download all data in tiles
+for (i in 9:length(bb_box_state_tile)){
 timestamp()
 print(paste("Starting",i, "from", length(bb_box_state_tile)))
 bb_box_state.m <- matrix (bb_box_state_tile [[i]], byrow = F, ncol = 2)
@@ -58,122 +57,21 @@ print(paste("Finished",i, "from", length(bb_box_state_tile)))
 timestamp()
 Sys.sleep(time=900)
 }
+# combine all tiles
+for (i in 1:16){
+  print(i)
+  if (i==1){
+    all_roads_c <- PA_all_roads_tile_1 %>% select(c("osm_id","highway"))
+  }
+  tmp <- get(paste0("PA_all_roads_tile_", i))%>% select(c("osm_id","highway"))
+  all_roads_c <- rbind(all_roads_c, tmp)
+}
+ggplot(all_roads_c %>% filter (highway=="primary"|highway=="motorway"|highway=="trunk")) +geom_sf()
 
+write_rds(all_roads_c, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/results/PA_roads_all_tiles.rds")
+write_sf(all_roads_c, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/results/PA_roads_all_tiles.geojson")
 
-
-
-#########################################################
-
-write_rds(PA_all_roads, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/processing/PA_roads.rds") 
-#PA_all_roads<-read_rds("/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/processing/PA_roads.rds")
-
-#MT 1
-state <- ne_states(country="Brazil", returnclass="sf") 
-state <- state %>% filter (code_hasc=="BR.MT")#| code_hasc=="BR.MT"| code_hasc=="BR.RO")
-ggplot()+geom_sf(data=state)
-bb_box_state <- st_bbox(state) 
-bb_box_state_tile <- split_bbox(bb_box_state,2,1)
-bb_box_state.m <- matrix (bb_box_state_tile[[1]], byrow = F, ncol = 2)
-rownames(bb_box_state.m)<- c("x","y")
-colnames(bb_box_state.m)<- c("min","max")
-
-str(bb_box_state.m)
-
-
-q <- bb_box_state.m%>%
-  opq()%>%
-  add_osm_features(
-    features = c ("\"highway\"=\"motorway\"",
-                  "\"highway\"=\"trunk\"",
-                  "\"highway\"=\"primary\"",
-                  "\"highway\"=\"secondary\"",
-                  "\"highway\"=\"tertiary\"",
-                  "\"highway\"=\"unclassified\"",
-                  "\"highway\"=\"track\"",
-                  "\"highway\"=\"road\""))
-
-MT1_all_roads <- osmdata_sf(q)
-
-MT1_all_roads_lines <- (MT1_all_roads$osm_lines)
-ggplot()+geom_sf(data=state) + geom_sf(data=MT1_all_roads$osm_lines)
-write_rds(MT1_all_roads, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/processing/MT1_roads.rds") 
-
-#MT2
-bb_box_state.m <- matrix (bb_box_state_tile[[2]], byrow = F, ncol = 2)
-rownames(bb_box_state.m)<- c("x","y")
-colnames(bb_box_state.m)<- c("min","max")
-
-str(bb_box_state.m)
-
-q <- bb_box_state.m%>%
-  opq()%>%
-  add_osm_features(
-    features = c ("\"highway\"=\"motorway\"",
-                  "\"highway\"=\"trunk\"",
-                  "\"highway\"=\"primary\"",
-                  "\"highway\"=\"secondary\"",
-                  "\"highway\"=\"tertiary\"",
-                  "\"highway\"=\"unclassified\"",
-                  "\"highway\"=\"track\"",
-                  "\"highway\"=\"road\""))
-
-MT2_all_roads <- osmdata_sf(q)
-
-MT2_all_roads_lines <- (MT2_all_roads$osm_lines)
-ggplot()+geom_sf(data=state) + geom_sf(data=MT2_all_roads$osm_lines)
-write_rds(MT2_all_roads, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/processing/MT2_roads.rds") 
-
-
-# RO
-state <- ne_states(country="Brazil", returnclass="sf") 
-state <- state %>% filter (code_hasc=="BR.RO")#| code_hasc=="BR.MT"| code_hasc=="BR.RO")
-#ggplot()+geom_sf(data=state)
-bb_box_state <- st_bbox(state) 
-bb_box_state.m <- matrix (bb_box_state, byrow = F, ncol = 2)
-rownames(bb_box_state.m)<- c("x","y")
-colnames(bb_box_state.m)<- c("min","max")
-
-str(bb_box_state.m)
-
-q <- bb_box_state.m%>%
-  opq()%>%
-  add_osm_features(
-    features = c ("\"highway\"=\"motorway\"",
-                  "\"highway\"=\"trunk\"",
-                  "\"highway\"=\"primary\"",
-                  "\"highway\"=\"secondary\"",
-                  "\"highway\"=\"tertiary\"",
-                  "\"highway\"=\"unclassified\"",
-                  "\"highway\"=\"track\"",
-                  "\"highway\"=\"road\""))
-
-RO_all_roads <- osmdata_sf(q)
-RO_all_roads_lines <- (RO_all_roads$osm_lines)
-ggplot()+geom_sf(data=state) + geom_sf(data=RO_all_roads$osm_lines)
-write_rds(RO_all_roads, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/processing/RO_roads.rds") 
-
-## plot them all
-state <- ne_states(country="Brazil", returnclass="sf") 
-
-state <- state %>% filter (code_hasc=="BR.RO"| code_hasc=="BR.MT"| code_hasc=="BR.RO")
-
-ggplot()+geom_sf(data=state) + 
-  geom_sf(data=RO_all_roads$osm_lines)+
-  geom_sf(data=MT1_all_roads$osm_lines)+
-  geom_sf(data=MT2_all_roads$osm_lines)+
-  geom_sf(data=PA_all_roads$osm_lines)
-
-
-## cleaning: 
-## combine and st_line_merge
-names(MT1_all_roads$osm_lines)
-names(MT2_all_roads$osm_lines)
-MT_roads <- rbind (MT1_all_roads$osm_lines %>% select(c("osm_id","highway")), MT2_all_roads$osm_lines %>%  select(c("osm_id","highway")))
-MT_roads_merge <- MT_roads %>% st_union()
-
-OSM_roads <- rbind(MT_roads_merge,PA_all_roads$osm_lines %>% select(c("osm_id","highway")), RO_all_roads$osm_lines %>%  select(c("osm_id","highway")))
-OSM_roads_merge <- OSM_roads %>% st_union()
-write_rds(OSM_roads_merge, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/results/OSM_roads.rds") 
-
-  
-
+# union by feature with st_union
+all_roads_c_u <- all_roads_c %>% st_union(by_feature=TRUE)
+write_rds(all_roads_c_u, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/results/PA_roads_all_tiles_u.rds")
+write_sf(all_roads_c_u, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/results/PA_roads_all_tiles_u.geojson")
