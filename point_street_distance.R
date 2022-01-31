604b6daa9cb24359f2bb9e534e4b2ed9cbdfa398
florian gollnow <floriangollnow@gess-vpn-1-a-135.intern.ethz.ch>
2022-01-31 18:19:40 -0500

    distance to roads

--- point_street_distance.R
+++ point_street_distance.R
@@ -17,7 +17,27 @@ roads_u <- roads %>%
 roads_u
 write_rds (roads_u, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/results/PA_roads_all_tiles_u2_s2off.rds")
 
-points1_10 <- points %>% filter(row<=9)
+points1_10 <- points %>% filter(row<=2)
 
+# any_road type
+point_to_road <- points %>% st_distance( roads_u, by_element = FALSE)
+point_to_road <- apply(point_roads2, 1,min)
+point_to_road.tb <- tibble(disRoads=point_roads)
+point_to_road.tb
+## highways and primary roads
+primary <- roads_u %>% filter (highway== "motorway"|highway== "trunk"|highway== "primary"| highway== "primary_link")
+point_to_primary <- points %>% st_distance( primary, by_element = FALSE)
+point_to_primary <- apply(point_roads2, 1,min)
+point_to_primary.tb <- tibble (disPrimaryRoad= point_to_primary)
 
-point_roads <- points1_10 %>% st_distance( roads, by_element = TRUE) 
+## secondary roads
+secondary <- roads_u %>% filter (highway== "secondary")
+point_to_secondary <- points %>% st_distance( secondary, by_element = FALSE)
+point_to_secondary <- apply(point_roads2, 1,min)
+point_to_secondary.tb <- tibble (disPrimaryRoad= point_to_secondary)
+
+point_data <- cbind (points, point_to_road.tb, point_to_primary.tb, point_to_secondary.tb)
+point_data
+write_rds (point_data, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/results/point_road_dist_m.rds")
+write_sf (point_data, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/results/point_road_dist_m.geojson")
+
