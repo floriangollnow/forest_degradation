d8437d4cdfa7ae4be4d8edb2108fd8282da1adb7
florian gollnow <floriangollnow@gess-vpn-1-a-163.intern.ethz.ch>
2022-02-01 10:45:00 -0500

    point_incra intersect

--- 2_point_incra_intersection.R
+++ 2_point_incra_intersection.R
@@ -2,7 +2,9 @@
 
 library(tidyverse)
 library(sf)
+sf_use_s2(FALSE)
 library(lubridate)
+library(nngeo)
 
 dir_as <- "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/assentamiento"
 dir_p <- "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/Points"
@@ -20,9 +22,55 @@ point_row <- read_rds (file.path(dir_p, "points_row_sf.rds"))
 #intersect
 assF_d <- assF_d %>% st_transform(crs=st_crs(point_row))
 assR_d <- assR_d %>% st_transform(crs=st_crs(point_row))
-point_assF <- point_row %>% st_intersection (assF_d)
-point_assR <- point_row %>% st_intersection (assR_d)
 
-point_row_result <- point_row %>% left_join(point_assF, by = 'row') %>% left_join(point_assR, by = 'row')
-write_rds (point_row_results , file.path(dir_as, "results/point_ass_intersect.rds"))
-write_sf (point_row_results , file.path(dir_as, "results/point_ass_intersect.geojson"))
+assF_d <- assF_d %>% st_crop(st_bbox(point_row))
+assF_d <- assF_d %>%  nngeo::st_remove_holes()
+assF_d <- assF_d %>%  st_make_valid()
+
+assR_d <- assR_d %>% st_crop(st_bbox(point_row))
+assR_d <- assR_d %>%  nngeo::st_remove_holes()
+assR_d <- assR_d %>%  st_make_valid()
+
+
+#in 10 tiles
+# assF
+ind <- point_row %>% nrow()/10
+for (i in 1:10){
+  print(i)
+  point_a <- point_row %>% filter (row >= ind*(i-1) & row< ind*i)
+  point_assF <- point_a %>% st_intersection (assF_d)
+  write_rds (point_assF, file.path (dir_as , paste0("processing/","point_assF_d",i,"_sf.rds")))
+  assign(paste0("point_assF_d_",i), point_assF)
+}
+for (i in 1:10){
+  if (i==1){
+    point_assF_all <- read_rds (file.path(dir_as,paste0("processing/","point_assF_d",i,"_sf.rds") ))
+  }else {
+    tmp <- read_rds (file.path(dir_as,paste0("processing/","point_assF_d",i,"_sf.rds") ))
+    point_assF_all <- rbind(point_assF_all, tmp)
+  }
+}
+#assR
+for (i in 1:10){
+  print(i)
+  point_a <- point_row %>% filter (row >= ind*(i-1) & row< ind*i)
+  point_assR <- point_a %>% st_intersection (assR_d)
+  write_rds (point_assR, file.path (dir_as , paste0("processing/","point_assR_d",i,"_sf.rds")))
+  assign(paste0("point_assF_d_",i), point_assR)
+}
+for (i in 1:10){
+  if (i==1){
+    point_assR_all <- read_rds (file.path(dir_as,paste0("processing/","point_assR_d",i,"_sf.rds") ))
+  }else {
+    tmp <- read_rds (file.path(dir_as,paste0("processing/","point_assR_d",i,"_sf.rds") ))
+    point_assR_all <- rbind(point_assR_all, tmp)
+  }
+}
+point_assF <- point_assF %>% select(-data_de_cr)
+point_assR <- point_assR %>% select(-data_de_cr)
+
+
+point_row_result <- point_row %>% left_join(point_assF %>% as_tibble() %>% select(-geometry), by = 'row') %>% 
+  left_join(point_assR %>% as_tibble() %>% select(-geometry), by = 'row')
+write_rds (point_row_result , file.path(dir_as, "results/point_ass_intersect.rds"))
+write_sf (point_row_result , file.path(dir_as, "results/point_ass_intersect.geojson"))
