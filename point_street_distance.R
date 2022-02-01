24ff21a9e230c8f0ea60abafdcd3d5fa8dbc959e
florian gollnow <floriangollnow@gess-vpn-1-a-163.intern.ethz.ch>
2022-02-01 08:46:34 -0500

    roads

--- point_street_distance.R
+++ point_street_distance.R
@@ -19,24 +19,16 @@ write_rds (roads_u, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/re
 
 points1_10 <- points %>% filter(row<=2)
 
+#https://gis.stackexchange.com/questions/225102/calculate-distance-between-points-and-nearest-polygon-in-r
 # any_road type
+timestamp()
+
 point_to_road <- points %>% st_distance( roads_u, by_element = FALSE)
-point_to_road <- apply(point_roads2, 1,min)
-point_to_road.tb <- tibble(disRoads=point_roads)
-point_to_road.tb
-## highways and primary roads
-primary <- roads_u %>% filter (highway== "motorway"|highway== "trunk"|highway== "primary"| highway== "primary_link")
-point_to_primary <- points %>% st_distance( primary, by_element = FALSE)
-point_to_primary <- apply(point_roads2, 1,min)
-point_to_primary.tb <- tibble (disPrimaryRoad= point_to_primary)
+point_to_road.tb <- point_to_road %>% as_tibble()
+names(point_to_road.tb) <- paste0("dist_",unique(roads_u$highway))
+timestamp()
 
-## secondary roads
-secondary <- roads_u %>% filter (highway== "secondary")
-point_to_secondary <- points %>% st_distance( secondary, by_element = FALSE)
-point_to_secondary <- apply(point_roads2, 1,min)
-point_to_secondary.tb <- tibble (disPrimaryRoad= point_to_secondary)
-
-point_data <- cbind (points, point_to_road.tb, point_to_primary.tb, point_to_secondary.tb)
+point_data <- cbind (points, point_to_road.tb)
 point_data
 write_rds (point_data, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/results/point_road_dist_m.rds")
 write_sf (point_data, "/Users/floriangollnow/Dropbox/ZDC_project/FEDE/OSM_roads/results/point_road_dist_m.geojson")
